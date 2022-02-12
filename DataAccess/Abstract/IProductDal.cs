c110e3d45f7e2145ca32b8697d2e7483e5f03d0d
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-12 10:56:43 +0300

    Update IProductDal.cs

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -7,9 +7,6 @@ namespace DataAccess.Abstract
 {
     public interface IProductDal : IEntityRepository<Product>
     {
-        //Bu interface'e bunu(List<Product> GetAll();) eklediğimizde yandaki sarı lambada Add reference to 'Entities' çıkar
-        //Başka bir katmanı kullanmak istediğimiz için referans veririz.
-        //ürünleri categoriye göre filtrele
         List<Product> GetAllByCategory(int categoryId);
     }
 }

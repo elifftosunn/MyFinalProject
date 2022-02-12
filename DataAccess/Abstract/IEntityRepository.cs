d976b006e6cfc2df3558e480b1e0b2eab3893099
elifftosunn <92747017+elifftosunn@users.noreply.github.com>
2022-02-12 10:56:22 +0300

    Update IEntityRepository.cs

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -6,17 +6,10 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    //generic constraint(Genel Kısıtlama)
-    //(where T:class,IEntity) => T ya IEntity olabilir ya da T, IEntity'den implemente olan birşey olabilir diyoruz.
-    // class : referans tip
-    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
-    // new() : new'lenebilir olmalıdır.
-    // IEntity INTERFACE OLDUGUNDAN  NEW'LENEMEZ.
-    public interface IEntityRepository<T> where T:class,IEntity,new()//class dediğimiz zaman referans tipli bu nedenle T yerine int,string yazamayız.
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
-        //Expression<Func<T,bool>> filter=null => FİLTRELEME(p=>p.productId=2)
-        List<T> GetAll(Expression<Func<T,bool>> filter=null);//filtre vermeyedebiliriz.Yani tüm data'yı verebiliriz
-        T Get(Expression<Func<T, bool>> filter);//burada filtre zorunlu
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void UpDate(T entity);
         void Delete(T entity);
